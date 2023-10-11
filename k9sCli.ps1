40eddb1686c8470dcccf10439766f11ea1607cba
simone.rigo <simone.rigo@lynxspa.com>
2023-10-11 18:11:07 +0200

    Ottimizzazioni & fix varie

--- k9sCli.ps1
+++ k9sCli.ps1
@@ -13,7 +13,7 @@ $OutputVariable  	 = $null
 $match_condition 	 = $null
 $global:apm_code	 = $null
 $global:filename 	 = $null
-$global:namespece	 = $null
+$global:namespace	 = $null
 $global:container_id = $null
 $global:find_message = "Sto cercando ."
 $global:cursorTop 	 = [Console]::CursorTop
@@ -29,7 +29,7 @@ $cluster_id = Read-Host "`nDigita il numero del cluster se lo conosci, 0 altrime
 $apm_code = $apm_code_in.ToLower()
 
 function get-List {
-    Write-Host "Azioni disponibili (riferite al namespace):`n"
+    Write-Host "`nAzioni disponibili (riferite al namespace):`n"
     Write-Host " 1  - Lista pods"
     Write-Host " 2  - Accesso al pod (Shell)"
     Write-Host " 3  - Rimuovi un Pod"
@@ -58,14 +58,14 @@ function get-List {
 }
 
 function printMsg {
-	Write-Host "Componente trovato nel cluster: " $filename -ForegroundColor White
+	Write-Host "Componente trovato nel cluster:" $filename -ForegroundColor White
 }
 
 function printFindMsg {
 	Clear-Host
 	$cursorTop 	 = [Console]::CursorTop
 	[Console]::SetCursorPosition(0, $cursorTop)
-	Write-Host "Namespace: " $namespece -ForegroundColor White "`n"
+	Write-Host "Namespace: " $namespace -ForegroundColor White "`n"
 	$frame = $frames[$global:counter % $frames.Length]
 	Write-Host "Sto cercando $frame" -NoNewLine
 	$global:counter += 1
@@ -73,16 +73,37 @@ function printFindMsg {
 
 function printPodList {
 	Write-Host "Per comodità ti elenco la lista dei Pod...."
-    kubectl --kubeconfig $filename --namespace=$namespece get pods
+    kubectl --kubeconfig $filename --namespace=$namespace get pods
+}
+
+function getConnection {
+	$rc = 0 #false
+	$localfn = (Get-Variable -Name filename -Scope Global).Value
+	$localns = (Get-Variable -Name namespace -Scope Global).Value
+	$command_str = -join("kubectl --kubeconfig ",$localfn," -n ",$localns," get pods")
+    $OutputVariable = (cmd.exe /c $command_str 2>&1) | Out-String
+    if(($OutputVariable -Notlike "*Error from server*") -and ($OutputVariable -Notlike "*No resources found*")) {
+   	    Clear-Host
+   		Write-Host "Namespace: " $namespace -ForegroundColor White "`n"
+   		printMsg
+        $rc = 1
+    }
+	return $rc
 }
 
 if(($ambiente -eq "dev") -or ($ambiente -eq "qa")){
-	$match_condition = '^(dev).*\.yaml$'
+	if ($apm_code -eq "dl00003") {
+        $match_condition = '^(dev).*_dl00003\.yaml$'
+	} else {
+		$match_condition = '^(dev-qa-)\d{1}\.yaml$'
+	}	
 } else {
-	$match_condition = '^(prod).*\.yaml$'
+	if ($apm_code -eq "dl00003") {
+		$match_condition = '^(prod_dl00003).*\.yaml$'
+	} else {
+		$match_condition = '^(prod-)\d{1}\.yaml$'
+	}
 }
-
-$sessions = Get-ChildItem 'C:\Users\simone.rigo\Enel\kubernates' | Where-Object { $_.Name -match $match_condition }
 
 if ($container_name.StartsWith($prefix)){
 	Write-Host "Trovato prefisso (pl). Lo elimino"
@@ -94,43 +115,32 @@ if ($container_name.StartsWith($prefix)){
 }
 
 if ($cluster_id -eq "0") {
-   foreach ($session in $sessions) {
-   	  $OutputVariable = $null
-   	  $namespece = -join("glin-",$apm_code,$container_name,"-",$ambiente,"-platform-namespace")
-   	  printFindMsg
-      $filename = ($session.Name.Substring($session.Name.LastIndexOf("\") + 1)).Replace("%20"," ")
-   	  $command_str = "kubectl --kubeconfig $filename -n $namespece get pods"
-      $OutputVariable = (cmd.exe /c $command_str 2>&1) | Out-String
-   	  #13Write-Host "`ncommand_str	--> " $command_str
-   	  #13Write-Host "filename	--> " $filename
-      #13Write-Host "OutputVariable	--> " $OutputVariable
-      if(($OutputVariable -Notlike "*Error from server*") -and ($OutputVariable -Notlike "*No resources found*")) {
-   		  Clear-Host
-   		  Write-Host "Namespace: " $namespece -ForegroundColor White "`n"
-   		  printMsg
-          break
-       }
-   	  #in alcuni casi nel cluster appare con il prefisso pl. Provo a cercarlo anche in questo modo
-   	  $namespece = -join("glin-",$apm_code,"pl",$container_name,"-",$ambiente,"-platform-namespace")
-   	  $command_str = "kubectl --kubeconfig $filename -n $namespece get pods"
-      $OutputVariable = (cmd.exe /c $command_str 2>&1) | Out-String
-      if(($OutputVariable -Notlike "*Error from server*") -and ($OutputVariable -Notlike "*No resources found*")) {
-   		  Clear-Host
-   		  Write-Host "Namespace: " $namespece -ForegroundColor White "`n"
-   		  printMsg
-          break
-         }
+    $sessions = Get-ChildItem 'C:\Users\simone.rigo\Enel\kubernates' | Where-Object { $_.Name -match $match_condition }
+    foreach ($session in $sessions) {
+   	    $OutputVariable = $null
+		Clear-Variable -Name namespace -Scope Global
+		Clear-Variable -Name filename -Scope Global
+   	    $namespace = -join("glin-",$apm_code,$container_name,"-",$ambiente,"-platform-namespace")
+	    Set-Variable -Name namespace -Value $namespace -Scope Global
+   	    printFindMsg
+        $filename = ($session.Name.Substring($session.Name.LastIndexOf("\") + 1)).Replace("%20"," ")
+		Set-Variable -Name filename -Value $filename -Scope Global
+   	    #in alcuni casi nel cluster appare con il prefisso pl. Provo a cercarlo anche in questo modo
+	    $rc = getConnection
+	    if ($rc) { break }
+   	    $namespace = -join("glin-",$apm_code,"pl",$container_name,"-",$ambiente,"-platform-namespace")
+	    Set-Variable -Name namespace -Value $namespace -Scope Global
+	    $rc = getConnection
+	    if ($rc) { break }
    }
 } else {
-	 $namespece = -join("glin-",$apm_code,$container_name,"-",$ambiente,"-platform-namespace")
-	 Write-Host "Namespace: " $namespece -ForegroundColor White "`n"
+	 $namespace = -join("glin-",$apm_code,$container_name,"-",$ambiente,"-platform-namespace")
+	 Write-Host "Namespace: " $namespace -ForegroundColor White "`n"
 	 $filename = -join("dev-qa-",$cluster_id,".yaml")
-	 $command_str = "kubectl --kubeconfig $filename -n $namespece get pods"
+	 $command_str = "kubectl --kubeconfig $filename -n $namespace get pods"
 	 $OutputVariable = (cmd.exe /c $command_str 2>&1) | Out-String
-	 if ($OutputVariable -Like "*No resources found*") {
-		  $namespece = -join("glin-",$apm_code,"pl",$container_name,"-",$ambiente,"-platform-namespace")
-   	 	  $command_str = "kubectl --kubeconfig $filename -n $namespece get pods"
-          $OutputVariable = (cmd.exe /c $command_str 2>&1) | Out-String
+	 if ($OutputVariable -Like "*fatal: cannot change to*") {
+		 Write-Host "`nRepository non trovato " -ForegroundColor Magenta
 	 }
 	 if(($OutputVariable -Notlike "*Error from server*") -and ($OutputVariable -Notlike "*No resources found*")) {
    		  Write-Host "Namespace: Componete trovato!`n"
@@ -150,29 +160,30 @@ while(1) {
 		$action = Read-Host "Seleziona un'azione"
 		Write-Host "`n"
 	}
+	#$namespace = (Get-Variable -Name namespace -Scope Global).Value
 	switch ($action)
 	{
 		0 { #Restituisce le scelte possibili
 			$action = $null
-			Write-Host "Namespace: " $namespece -ForegroundColor White "`n"
+			Write-Host "Namespace: " $namespace -ForegroundColor White "`n"
 			get-List
 		}
 		1 { #Lista pods
-			kubectl --kubeconfig $filename --namespace=$namespece get pods
+			kubectl --kubeconfig $filename --namespace=$namespace get pods
 			$action = Read-Host "`nVuoi ulteriori dettagli (rete/Host) dei pod (y/n)?"
 			if ($action -eq "y") {
-				kubectl --kubeconfig $filename --namespace=$namespece get pods --show-labels=true -o wide
+				kubectl --kubeconfig $filename --namespace=$namespace get pods --show-labels=true -o wide
 			}			
 		}
 		2 { #Accesso al container (Shell)
 			$container_id = Read-Host "`nInserisci l'id del container(NAME)"
-			kubectl --kubeconfig $filename -n $namespece exec -it $container_id -c $container_name -- sh -c "clear; (bash || ash || sh)"
+			kubectl --kubeconfig $filename -n $namespace exec -it $container_id -c $container_name -- sh -c "clear; (bash || ash || sh)"
 		}
 		3 { #Rimuovi un Pod
 			$container_id = Read-Host "`nInserisci l'id del container(NAME)"
 			$action = Read-Host "`nSei sicuro di voler cancellare il pod(y/n)?"
 			if ($action -eq "y") {
-				kubectl --kubeconfig $filename -n $namespece delete pod $container_id
+				kubectl --kubeconfig $filename -n $namespace delete pod $container_id
 			}
 			$action = $null
 		}
@@ -182,17 +193,17 @@ while(1) {
 			if ($event_type -eq "y") {
 				printPodList
 				$pod_id = Read-Host "`nInserisci l'id del pod che vuoi ispezionare"
-				kubectl --kubeconfig $filename -n $namespece describe pod/$pod_id
+				kubectl --kubeconfig $filename -n $namespace describe pod/$pod_id
 			} else {
-				kubectl --kubeconfig $filename -n $namespece get pods --output=yaml
+				kubectl --kubeconfig $filename -n $namespace get pods --output=yaml
 			}			
 		}
 		5 { #Visualizza i container di un namespace
 			Write-Host "Lista container container...."
-			kubectl --kubeconfig $filename --namespace=$namespece get pod -o="custom-columns=NAME:.metadata.name,INIT-CONTAINERS:.spec.initContainers[*].name,CONTAINERS:.spec.containers[*].name"
+			kubectl --kubeconfig $filename --namespace=$namespace get pod -o="custom-columns=NAME:.metadata.name,INIT-CONTAINERS:.spec.initContainers[*].name,CONTAINERS:.spec.containers[*].name"
             $dettagli = Read-Host "`nDesideri visualizzare i relativi account aws utilizzati(y/n)?"
 			if ($dettagli -eq "y") {
-				kubectl --kubeconfig $filename -n $namespece get pods -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{":\t"}{range .spec.containers[*]}{.image}{", "}{end}{end}' | Sort-Object
+				kubectl --kubeconfig $filename -n $namespace get pods -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{":\t"}{range .spec.containers[*]}{.image}{", "}{end}{end}' | Sort-Object
 			}
 			$dettagli = $null
 		} # kubectl --kubeconfig dev-qa-1.yaml --namespace=glin-ap31312pltm004-dev-platform-namespace get pods -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{":\n"}{range .spec.containers[*]}{.image}{"\n"}{end}{end}' -l app=tm004
@@ -202,32 +213,32 @@ while(1) {
 			$pod_id = Read-Host "`nInserisci l'id del pod(NAME)"
 			$row_num = Read-Host "Numero di righe (ultime) che vuoi visualizzare?"
 			$container_name = Read-Host "`nInserisci il nome del container"
-			kubectl --kubeconfig $filename logs $pod_id -n $namespece  --tail=$row_num -c $container_name
+			kubectl --kubeconfig $filename logs $pod_id -n $namespace  --tail=$row_num -c $container_name
 		}
 		7 { #Lista secrets del pod (filtro namespace)
-			kubectl --kubeconfig $filename -n $namespece get secrets
+			kubectl --kubeconfig $filename -n $namespace get secrets
 		}
 		8 { #Lista secretproviderclass del pod (filtro namespace) - NON UTILIZZABILE(GRANT)
-			kubectl --kubeconfig $filename -n $namespece get secretproviderclass
+			kubectl --kubeconfig $filename -n $namespace get secretproviderclass
 		}
 		9 { #Lista secrets del cluster
 			kubectl --kubeconfig $filename -n kube-system get secrets
 		}
 		10 { #Descrivo il secret
-			kubectl --kubeconfig $filename -n $namespece get secrets
+			kubectl --kubeconfig $filename -n $namespace get secrets
 			$secret_id = Read-Host "`nInserisci l'id del secret"
-			kubectl --kubeconfig $filename -n $namespece describe secrets $secret_id
+			kubectl --kubeconfig $filename -n $namespace describe secrets $secret_id
 			$dettagli  = Read-Host "`nDesideri ulteriori dettagli (y/n)?"
 			if ($dettagli -eq "y") {
-				kubectl --kubeconfig $filename -n $namespece get secrets $secret_id -o yaml
+				kubectl --kubeconfig $filename -n $namespace get secrets $secret_id -o yaml
 			}
 		}
 		11 { #Lista degli eventi
 			$event_type = Read-Host "Vuoi visualizzare solo gli eventi di tipo error/warning(y/n)?"
 			if ($event_type -eq "y") {
-				kubectl --kubeconfig $filename -n $namespece get events --field-selector type!=Normal
+				kubectl --kubeconfig $filename -n $namespace get events --field-selector type!=Normal
 			} else {
-				kubectl --kubeconfig $filename -n $namespece get events
+				kubectl --kubeconfig $filename -n $namespace get events
 			}
 		}
 
@@ -235,22 +246,22 @@ while(1) {
 			printPodList
 			$pod_id = Read-Host "`nInserisci l'id del pod(NAME)"
 			#Devo passare per una stringa altrimenti il comando fallisce. Assurdo!
-			$command_str = "kubectl --kubeconfig $filename --namespace=$namespece get events --field-selector involvedObject.kind=Pod,involvedObject.name=$pod_id"
+			$command_str = "kubectl --kubeconfig $filename --namespace=$namespace get events --field-selector involvedObject.kind=Pod,involvedObject.name=$pod_id"
 			cmd.exe /c $command_str
 		}
 		13 { #Statistiche Keda del namespace associato
 			Write-Host "visualizza scaledobject...."
-			kubectl --kubeconfig $filename -n $namespece get scaledobject
+			kubectl --kubeconfig $filename -n $namespace get scaledobject
 			Write-Host "visualizza ScaledJobs...."
-			kubectl --kubeconfig $filename -n $namespece get ScaledJobs
+			kubectl --kubeconfig $filename -n $namespace get ScaledJobs
 		}
 		14 { #Lista dei services del namespace associato
-			kubectl --kubeconfig $filename -n $namespece get svc
+			kubectl --kubeconfig $filename -n $namespace get svc
 		}
 		15 { #Metriche del Pod
 			printPodList
 			$pod_id = Read-Host "`nInserisci l'id del pod(NAME)"
-			kubectl --kubeconfig $filename -n $namespece describe PodMetrics $pod_id
+			kubectl --kubeconfig $filename -n $namespace describe PodMetrics $pod_id
 		}
 		16 { #Verifica i servizi attivi
 			kubectl --kubeconfig $filename -n istio-system get svc
@@ -262,25 +273,25 @@ while(1) {
 			kubectl --kubeconfig $filename -n istio-system -l istio=ingressgateway get svc
 		}
 		19 { #Visualizza il services del cluster/namespace selezionato
-			kubectl --kubeconfig $filename --namespace $namespece get services
+			kubectl --kubeconfig $filename --namespace $namespace get services
 		}
 		20 { #Visualizza lo stato di hpa
-			kubectl --kubeconfig $filename --namespace $namespece get hpa
+			kubectl --kubeconfig $filename --namespace $namespace get hpa
 		}
 		21 { #Trasferisci un file dal Pod in locale
 			printPodList
 			$pod_id = Read-Host "`nInserisci l'id del pod che vuoi ispezionare"
 			$file_path = Read-Host "`nInserisci il path assoluto e il nome del file es: /dirName/dirName/fileName.txt"
 			$file_path_locale = Read-Host "`nInserisci il nome del file locale senza path (verrà scritto nel dir di esecuzione dello script all'inteno di una dir con lo stesso nome)"
-			$input_file = -join($namespece,"/",$pod_id,":",$file_path)
+			$input_file = -join($namespace,"/",$pod_id,":",$file_path)
 			kubectl cp $input_file $file_path_locale --kubeconfig $filename -c $container_name
 		}
 		22 { #Lista jobs
-			kubectl --kubeconfig $filename -n $namespece get job
+			kubectl --kubeconfig $filename -n $namespace get job
 		}
 		23 { #Descrivi job
 			$job_id = Read-Host "`nInserisci l'id del job che vuoi ispezionare"
-			kubectl --kubeconfig $filename -n $namespece describe jobs/$job_id
+			kubectl --kubeconfig $filename -n $namespace describe jobs/$job_id
 		}
 		24 { #Nessuna - esci
 			exit
